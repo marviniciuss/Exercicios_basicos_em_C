199a4f87e8f36cb5e8cc90f9c16a429deedba746
Marcus Vinicius Guimarães <162352556+marviniciuss@users.noreply.github.com>
2024-05-18 12:42:07 -0300

    Update Jogo_de_adivinhacao.c

--- Variados/Jogo_de_adivinhacao.c
+++ Variados/Jogo_de_adivinhacao.c
@@ -21,6 +21,9 @@ int main () {
 	int nivel;
 	int total_de_tentativas;
 	int acertou=0;
+	int bis;
+	
+	do { //loop que permite o usuário jogar novamente
 	
 	printf("Qual o nível de dificuldade?\n");
 	printf("(1) Fácil (2) Médio (3) Difícil \n\n");
@@ -42,32 +45,42 @@ int main () {
 			return 0;
 	}
 	
+	int memoria=1000;
+	
 	//INÍCIO DO LOOP
 	for (i;i<total_de_tentativas;i++){
 	printf("-> Tentativa %d de %d\n", i+1, total_de_tentativas);
 	printf("Chute um número: ");
 	scanf("%d", &chute);
 	printf("\n");
-		//Verifica se o número digitado foi negativo
-		if (chute<0){
-			printf("Você não pode chutar números negativos.\n");
-			i--;
-			continue; //Essa função interrompe e recomeça o loop. É diferente da função break que sai completamente de loop;
+	if (memoria==chute){
+		printf("Você já chutou esse número! Tente outra vez.");
+		i--; 
+	} else {
+			//Verifica se o número digitado foi negativo
+			if (chute<0){
+				printf("Você não pode chutar números negativos.\n");
+				i--;
+				continue; //Essa função interrompe e recomeça o loop. 
+				         //É diferente da função break que sai completamente de loop;
+			}
+	    // a variável "acertou" recebe o valor 1 (verdadeiro) se a condição for verdadeira e 
+		//recebe o valor 0 (falso) se a condição for falsa;
+		acertou = chute == numerosecreto; 
+		int maior = chute > numerosecreto; //a variável maior assume valores 0 (condição falsa) ou 1 (condição verdadeira);
+		int menor = chute < numerosecreto; //a variável menor assume valores 0 (condição falsa) ou 1 (condição verdadeira);
+		
+		if (acertou){  // somente executa o if se acertou for 1 (verdadeiro);         
+			break;
+		} else if (maior){
+			printf("Seu chute foi maior do que o número secreto!\n");
+		} else if (menor){
+			printf("Seu chute foi menor do que o número secreto!\n");
 		}
-    // a variável "acertou" recebe o valor 1 (verdadeiro) se a condição for verdadeira e recebe o valor 0 (falso) se a condição for falsa;
-	acertou = chute == numerosecreto; 
-	int maior = chute > numerosecreto; //a variável maior assume valores 0 (condição falsa) ou 1 (condição verdadeira);
-	int menor = chute < numerosecreto; //a variável menor assume valores 0 (condição falsa) ou 1 (condição verdadeira);
-	
-	if (acertou){  // somente executa o if se acertou for 1 (verdadeiro);         
-		break;
-	} else if (maior){
-		printf("Seu chute foi maior do que o número secreto!\n");
-	} else if (menor){
-		printf("Seu chute foi menor do que o número secreto!\n");
-	}
-	pontos=pontos - (double)abs((double)chute-(double)numerosecreto)/2;
-	} //FIM DO LOOP
+		pontos=pontos - (double)abs((double)chute-(double)numerosecreto)/2;
+		
+		memoria=chute; //atualiza o valor da variável memória 
+	}} //FIM DO LOOP
 	
 	if (acertou){
 		printf("                                                  \n");
@@ -108,6 +121,11 @@ int main () {
 		printf("O número secreto era %d \n", numerosecreto);
 	}
 	printf("\nSua pontuação foi %.2f pontos! \n", pontos);
+	
+	printf("\n\n Digite 1 se quiser jogar novamente! :) -> ");
+	scanf("%d",&bis);
+	} while (bis == 1); //Se o usuário digitar 1, o jogo recomeça.
+	
 
 return 0;
 }
