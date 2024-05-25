dbb6b2d298e34abbe673815ec0c6478c481d93be
Marcus Vinicius Guimarães <162352556+marviniciuss@users.noreply.github.com>
2024-05-25 13:55:48 -0300

    Add files via upload

---
+++ JOGO_DA_FORCA/forca.h
@@ -0,0 +1,12 @@
+#define TAMANHO_PALAVRA 20
+
+void abertura();
+void escolhepalavra();
+void chuta();
+int jachutou(char letra);
+void desenhaforca();
+int letraexiste(char letra);
+int chuteserrados();
+int enforcou();
+int ganhou();
+void adicionapalavra();
